47e03648d7772add3d8ca379a86b17a8a5ea0cc7
Devansh Sadhya <180571620+DevanshSadhya@users.noreply.github.com>
2025-12-25 17:26:21 +0530

    Delete Devansh Sadhya Java directory
    Signed-off-by: Devansh Sadhya <180571620+DevanshSadhya@users.noreply.github.com>

--- Devansh Sadhya Java/MultiplePrintAddress/src/MultiplePrintAddress.java
+++
@@ -1,7 +0,0 @@
-public class MultiplePrintAddress {
-    public static void main(String[] args) {
-        System.out.println("Devansh Sadhya");
-        System.out.println("Stanza Athens");
-        System.out.println("Greater Noida");
-    }
-}
--- Devansh Sadhya Java/SinglePrintAddress/src/SinglePrintAddress.java
+++
@@ -1,6 +0,0 @@
-import  java.util.Scanner;
-public class SinglePrintAddress {
-    public static void main(String[] args) {
-        System.out.print("This is single print address");
-    }
-}
