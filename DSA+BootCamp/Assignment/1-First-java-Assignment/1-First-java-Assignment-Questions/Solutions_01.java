def099f42fd378f48de614e39a6d6e7f86ad700e
DevanshSadhya <devanshsadhya77@gmail.com>
2025-12-27 20:18:17 +0530

    Add solution file for first Java assignment

--- DSA+BootCamp/Assignment/1-First-java-Assignment/1-First-java-Assignment-Questions/Solutions_01.java
+++ DSA+BootCamp/Assignment/1-First-java-Assignment/1-First-java-Assignment-Questions/Solutions_01.java
@@ -1,8 +1,10 @@
 //This is the soltion file for first java assignment
+//1.Write a program to print whether a number is even or odd, also take input from the user.
+
 import java.util.*; 
-public class Solutions {
+
+public class Solutions_01 {
     public static void main(String[] args) {
-        //1.Write a program to print whether a number is even or odd, also take input from the user.
         Scanner input = new Scanner(System.in);
         //getting input from user
         System.out.println("Enter a number: ");
